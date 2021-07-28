f66aec01cebce8d2c5846147e8f7e1f24b3532ad
Lisa Kim <lisa@goteleport.com>
2021-07-28 15:55:00 -0700

    Rebase changes

--- lib/services/usertoken_test.go
+++ lib/services/usertoken_test.go
@@ -20,7 +20,7 @@ import (
 
 	"gopkg.in/check.v1"
 
-	"github.com/gravitational/teleport/api/defaults"
+	"github.com/gravitational/teleport/api/v7/defaults"
 	"github.com/gravitational/teleport/api/v7/types"
 	"github.com/gravitational/teleport/lib/fixtures"
 )
